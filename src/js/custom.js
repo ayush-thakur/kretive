45189541b844f4a89d9e5041788a558a5c080faa
ayush-thakur <thakurayush511@gmail.com>
2020-08-17 11:48:53 +0530

    abot

---
+++ src/js/custom.js
@@ -0,0 +1,13 @@
+
+$('.slider-image').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<img id="nextt" src="./src/images/arrowl.png"></img>',
+    prevArrow: '<img id="prevv" src="./src/images/arrowr.png"></img>',
+
+  });

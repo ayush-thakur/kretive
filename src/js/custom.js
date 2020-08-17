4356a855c943584a7533d6343489bd1b094e5f09
ayush-thakur <thakurayush511@gmail.com>
2020-08-17 16:21:18 +0530

    home page done

--- src/js/custom.js
+++ src/js/custom.js
@@ -11,3 +11,24 @@ $('.slider-image').slick({
     prevArrow: '<img id="prevv" src="./src/images/arrowr.png"></img>',
 
   });
+
+  $('.testimonial_owlCarousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    autoplay:false,   
+    smartSpeed: 3000, 
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
